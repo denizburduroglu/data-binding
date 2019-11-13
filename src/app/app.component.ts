8b8b6212fc1692ded0a657f3e9b1ce4951e51290
denizburduroglu <denizburduroglu@gmail.com>
2019-11-13 16:03:25 -0500

    Select highlight directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'data-binding';
   someValue = "givevalue";
+  customers = [{"fname": "FName1","lname": "LName1",},
+  {"fname": "FName2","lname": "LName2",},
+  {"fname": "FName3","lname": "LName3",},
+  {"fname": "FName4","lname": "LName4",},
+  {"fname": "FName5","lname": "LName5",}
+]
   submit(event) {
     // debugger;
     console.log(event.value);
     this.someValue = event.value;
   }
+
+  flag = true;
+  toggle() {
+    this.flag = !this.flag;
+  }
+
+  setColor(color) {
+    console.log(color);
+  }
+
 }
